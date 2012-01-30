624a9da68a9ce7fa352cfff77932d559d1afb9ae
joernchen of Phenoelit <joernchen@phenoelit.de>
2012-01-30 10:06:05 +0100

    Cookie sign script

---
+++ ruby/sign-cookie.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/ruby
+# Sign a cookie in RoR style
+require 'base64' 
+require 'openssl'
+require 'optparse'
+
+banner = "Usage: #{$0} -k KEY [-c COOKIE]\nCookie is a raw ruby expression like '{:user_id => 1}'"
+
+hashtype = 'SHA1'
+key = nil
+cookie = {"user_id"=>1}
+
+opts = OptionParser.new do |opts|
+  opts.banner = banner
+  opts.on("-k", "--key KEY") do |h|
+    key = h
+  end
+  opts.on("-c", "--cookie COOKIE") do |w|
+    cookie = w
+  end  
+end
+
+begin
+ opts.parse!(ARGV) 
+rescue Exception => e
+  puts e, "", opts
+  exit
+end 
+
+if key.nil?
+  puts banner 
+  exit
+end
+
+
+cook = Base64.encode64(Marshal.dump(eval("#{cookie}"))).chomp
+
+digest = OpenSSL::HMAC.hexdigest(OpenSSL::Digest::Digest.new(hashtype), key, cook)
+
+puts("#{cook}--#{digest}") 
